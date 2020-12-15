4b98fba1f0087a06dad46be1efcfca00db70d841
Jan Kühle <jan.kuehle@codecentric.de>
2020-12-15 16:37:12 +0100

    Default prettier style

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 import {
   useQuery,
   useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from 'react-query'
-import { dehydrate, Hydrate } from 'react-query/hydration'
-import { ReactQueryDevtools } from 'react-query/devtools'
+} from "react-query";
+import { dehydrate, Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const getCharacters = async () => {
-  await new Promise(r => setTimeout(r, 500))
-  const { data } = await axios.get('https://rickandmortyapi.com/api/character/')
-  return data
-}
+  await new Promise((r) => setTimeout(r, 500));
+  const { data } = await axios.get(
+    "https://rickandmortyapi.com/api/character/"
+  );
+  return data;
+};
 
-const getCharacter = async selectedChar => {
-  await new Promise(r => setTimeout(r, 500))
+const getCharacter = async (selectedChar) => {
+  await new Promise((r) => setTimeout(r, 500));
   const { data } = await axios.get(
     `https://rickandmortyapi.com/api/character/${selectedChar}`
-  )
-  return data
-}
+  );
+  return data;
+};
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function CustomApp({ pageProps }) {
   return (
@@ -32,43 +34,43 @@ export default function CustomApp({ pageProps }) {
         <Example />
       </Hydrate>
     </QueryClientProvider>
-  )
+  );
 }
 
 CustomApp.getInitialProps = async () => {
   await Promise.all([
-    queryClient.prefetchQuery('characters', getCharacters),
-    queryClient.prefetchQuery(['character', 1], () => getCharacter(1)),
-  ])
+    queryClient.prefetchQuery("characters", getCharacters),
+    queryClient.prefetchQuery(["character", 1], () => getCharacter(1)),
+  ]);
 
   return {
     pageProps: {
       dehydratedState: dehydrate(queryClient),
     },
-  }
-}
+  };
+};
 
 function Example() {
-  const client = useQueryClient()
-  const [selectedChar, setSelectedChar] = React.useState(1)
+  const client = useQueryClient();
+  const [selectedChar, setSelectedChar] = React.useState(1);
 
-  const { data } = useQuery('characters', getCharacters, {
+  const { data } = useQuery("characters", getCharacters, {
     refetchOnWindowFocus: false,
     staleTime: 10_000,
-  })
+  });
 
   const { data: selectedData } = useQuery(
-    ['character', selectedChar],
+    ["character", selectedChar],
     () => getCharacter(selectedChar),
     {
       refetchOnWindowFocus: false,
       staleTime: 10_000,
     }
-  )
+  );
 
   const invalidate = () => {
-    client.invalidateQueries('characters')
-  }
+    client.invalidateQueries("characters");
+  };
 
   return (
     <div className="App">
@@ -81,18 +83,18 @@ function Example() {
       <button onClick={invalidate}>Invalidate</button>
       <h2>Characters</h2>
       <ul>
-        {data?.results.map(char => (
+        {data?.results.map((char) => (
           <li
             key={char.id}
             onClick={() => {
-              setSelectedChar(char.id)
+              setSelectedChar(char.id);
             }}
           >
             <div
               style={
-                client.getQueryData(['character', char.id])
+                client.getQueryData(["character", char.id])
                   ? {
-                      fontWeight: 'bold',
+                      fontWeight: "bold",
                     }
                   : {}
               }
@@ -108,5 +110,5 @@ function Example() {
       </p>
       <ReactQueryDevtools initialIsOpen />
     </div>
-  )
+  );
 }
